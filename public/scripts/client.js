86a9423e57435e4b59a6304c4e3be9302b889767
Kenny Lee <kenny.lee927@gmail.com>
2023-09-09 07:41:26 -0400

    write loadTweets function, move eventhandler to module, timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac",
-    },
-    "content": {
-      "text":
-        "If I have seen further it is by standing on the shoulders of giants",
-    },
-    "created_at": 1461116232227,
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd",
-    },
-    "content": {
-      "text": "Je pense , donc je suis",
-    },
-    "created_at": 1461113959088,
-  },
-];
 // converts each tweet object to html and adds it to the page
 const renderTweets = function (tweets) {
   tweets.forEach((tweet) => {
@@ -54,7 +27,7 @@ const createTweetElement = function (tweet) {
       <div class="tweetContent">${tweet.content.text}</div>
     
       <footer>
-        <div>${tweet.created_at}</div>
+        <div>${timeago.format(tweet.created_at)}</div>
         <div class="tweetActions">
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -66,12 +39,23 @@ const createTweetElement = function (tweet) {
   return $tweet;
 };
 
-// render tweets when DOM is ready to be modified
-$(document).ready(function () {
-  renderTweets(data);
+const addFormEventHandler = function () {
   $("form").on("submit", function () {
     event.preventDefault();
     const seralizedData = $(this).serialize();
     $.ajax("/tweets/", { method: "POST", data: seralizedData });
   });
+};
+
+const loadTweets = function () {
+  $.ajax("/tweets/", { method: "GET" }).then(function (moreTweets) {
+    renderTweets(moreTweets);
+  });
+};
+
+loadTweets();
+
+// render tweets when DOM is ready to be modified
+$(document).ready(function () {
+  addFormEventHandler();
 });
