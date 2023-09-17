d56cddca695e22b73dbc655ccb911ec065b5b6bf
Kenny Lee <kenny.lee927@gmail.com>
2023-09-16 23:12:58 -0400

    update comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $("#tweet-text").on("input", function () {
-    // assign div to variable
+    // assign character counter div to variable
     const newTweetCounter = $(this).siblings().find(".counter");
-    // update value
+    // update value on input
     newTweetCounter.text(140 - this.value.length);
-    // color check
+    // color check (text vs number type coercion)
     if (newTweetCounter.text() < 0) {
       newTweetCounter.addClass("red");
     } else {
